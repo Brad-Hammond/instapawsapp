4c8c6be0d6f36420c24a2bfbd6d6ea0a17eba1e0
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 21:41:14 +0000

    Feat: Added Navbar with React-Bootstrap and - Router links.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { AiOutlineUser, AiOutlineLogout } from "react-icons/ai";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 const NavBar = () => {
     return <div>navbar placeholder</div>;
