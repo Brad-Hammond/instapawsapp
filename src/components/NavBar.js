fbf4145d02a06db93750537489914691fd04fe5e
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 21:42:55 +0000

    Feat: Added placeholder NavBar component with - dummy user state.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 
 const NavBar = () => {
-    return <div>navbar placeholder</div>;
-};
+    const currentUser = null;
+    const setCurrentUser = () => {};
+}
 
 export default NavBar;
