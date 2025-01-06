2a6ceecf374a7252dd1ada9991c797bd1f77739c
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 21:58:40 +0000

    Feat: Added placeholder components for app routes.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import NavBar from "./components/NavBar";
 import './api/axiosDefault';
 
+// Placeholder Components for Routes
+const Home = () => <div>Home Page</div>;
+const Login = () => <div>Login Page</div>;
+const SignUp = () => <div>Sign Up Page</div>;
+const CreatePost = () => <div>Create Post Page</div>;
+const Profile = () => <div>Profile Page</div>;
+
 function App() {
   return (
     <div className="App">
