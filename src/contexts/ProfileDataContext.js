5e0db78131713822da069e655d78db144a99f8c9
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 11:29:39 +0000

    Feat: Initialize ProfileDataProvider with state - management.

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,7 +1,22 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
 
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+    const [profileData, setProfileData] = useState({
+      profilePage: { results: [] },
+      popularProfiles: { results: [] },
+    });
+  
+    return (
+      <ProfileDataContext.Provider value={profileData}>
+        <SetProfileDataContext.Provider value={{ setProfileData }}>
+          {children}
+        </SetProfileDataContext.Provider>
+      </ProfileDataContext.Provider>
+    );
+  };
