e4379d139ca2a393003f91bfa17c80aa69bb35c8
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 16:06:40 +0000

    Feat: Added icons to Toolbar navigation links.

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { MdOutlineLibraryAdd, MdOutlineSportsHandball } from "react-icons/md";
+import { RiHeartsFill } from "react-icons/ri";
 
 const Toolbar = () => {
   const { pathname } = useLocation();
   return (
     <div>
       <Link to="/feed" className={pathname === "/feed" ? "active" : null}>
-        My Feed
+        <MdOutlineSportsHandball /> My Feed
       </Link>
       <Link to="/liked" className={pathname === "/liked" ? "active" : null}>
-        My Hearts
+        <RiHeartsFill /> My Hearts
       </Link>
       <Link
         to="/posts/create"
         className={pathname === "/posts/create" ? "active" : null}
       >
-        Create Post
+        <MdOutlineLibraryAdd /> Create Post
       </Link>
     </div>
   );
