d9cc365ceaa66bd293e1aa869728484b36bf21bf
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 11:35:29 +0000

    Feat: Added handleFollow functionality to update - followers.

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,4 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
+import { axiosRes } from "../api/axiosDefault";
+import { followHelper } from "../utils/utils";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
@@ -7,16 +9,41 @@ export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 export const ProfileDataProvider = ({ children }) => {
-    const [profileData, setProfileData] = useState({
-      profilePage: { results: [] },
-      popularProfiles: { results: [] },
-    });
-  
-    return (
-      <ProfileDataContext.Provider value={profileData}>
-        <SetProfileDataContext.Provider value={{ setProfileData }}>
-          {children}
-        </SetProfileDataContext.Provider>
-      </ProfileDataContext.Provider>
-    );
+  const [profileData, setProfileData] = useState({
+    profilePage: { results: [] },
+    popularProfiles: { results: [] },
+  });
+
+  const handleFollow = async (clickedProfile) => {
+    try {
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        profilePage: {
+          results: prevState.profilePage.results.map((profile) =>
+            followHelper(profile, clickedProfile, data.id)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            followHelper(profile, clickedProfile, data.id)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.error(err);
+    }
   };
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+};
