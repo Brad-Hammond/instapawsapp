cc071bbb676a4018f412e3881b341ce363c72ab0
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 14:49:38 +0000

    Feat: Updated App.js for signup form.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.module.css';
 import NavBar from "./components/NavBar";
 import LogInForm from "./pages/auth/LogInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
 import HomePage from "./components/HomePage";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import './api/axiosDefault';
 
 // Placeholder Components for Routes
-const SignUp = () => <div>Sign Up Page</div>;
 const CreatePost = () => <div>Create Post Page</div>;
 const Profile = () => <div>Profile Page</div>;
 
@@ -21,11 +21,11 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LogInForm />} />
-            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signup" element={<SignUpForm />} />
             <Route path="/posts/create" element={<CreatePost />} />
             <Route path="/profiles/:id" element={<Profile />} />
             <Route path="/dj-rest-auth/login" element={<LogInForm />} />
-            <Route path="/dj-rest-auth/registration" element={<SignUp />} />
+            <Route path="/dj-rest-auth/registration" element={<SignUpForm />} />
           </Routes>
         </CurrentUserProvider>
       </Router>
