26ffc184f1371aa036c0202d1b5da2cd0adeebc8
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 16:07:06 +0000

    Feat: Applied CSS styling to Toolbar component.

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,22 +2,26 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { MdOutlineLibraryAdd, MdOutlineSportsHandball } from "react-icons/md";
 import { RiHeartsFill } from "react-icons/ri";
+import styles from "../styles/Toolbar.module.css";
 
 const Toolbar = () => {
   const { pathname } = useLocation();
   return (
-    <div>
-      <Link to="/feed" className={pathname === "/feed" ? "active" : null}>
-        <MdOutlineSportsHandball /> My Feed
+    <div className={styles.Toolbar}>
+      <Link to="/feed" className={pathname === "/feed" ? styles.active : null}>
+        <MdOutlineSportsHandball className={styles.Icon} /> My Feed
       </Link>
-      <Link to="/liked" className={pathname === "/liked" ? "active" : null}>
-        <RiHeartsFill /> My Hearts
+      <Link
+        to="/liked"
+        className={pathname === "/liked" ? styles.active : null}
+      >
+        <RiHeartsFill className={styles.Icon} /> My Hearts
       </Link>
       <Link
         to="/posts/create"
-        className={pathname === "/posts/create" ? "active" : null}
+        className={pathname === "/posts/create" ? styles.active : null}
       >
-        <MdOutlineLibraryAdd /> Create Post
+        <MdOutlineLibraryAdd className={styles.Icon} /> Create Post
       </Link>
     </div>
   );
