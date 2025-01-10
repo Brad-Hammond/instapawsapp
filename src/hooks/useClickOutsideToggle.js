8a2565062708783446eb974d2cf0580b55f17b35
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 11:50:47 +0000

    Feat: Added useEffect to handle clicks outside - the referenced element.

--- src/hooks/useClickOutsideToggle.js
+++ src/hooks/useClickOutsideToggle.js
@@ -1,10 +1,23 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const useClickOutsideToggle = () => {
   const [expanded, setExpanded] = useState(false);
   const ref = useRef(null);
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setExpanded(false);
+      }
+    };
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+
   return { expanded, setExpanded, ref };
 };
 
 export default useClickOutsideToggle;
+
