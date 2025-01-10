20057b337e873c1a720a251c3a15dc6034f8fb9c
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 12:48:36 +0000

    Feat: Added state for form data and handleChange - function.

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LogInForm = () => {
+  const [logInData, setLogInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setLogInData({
+      ...logInData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <h1>Log In</h1>
       <form>
-        <input type="text" name="username" placeholder="Enter username" />
-        <input type="password" name="password" placeholder="Enter password" />
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter username"
+          value={logInData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          value={logInData.password}
+          onChange={handleChange}
+        />
         <button type="submit">Log In</button>
       </form>
     </div>
