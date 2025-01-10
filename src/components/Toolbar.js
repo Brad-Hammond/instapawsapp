1a9c1004ac379fea56bac7fdbf8f44fc7ad3e9c2
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 16:05:33 +0000

    Feat: Added navigation links to Toolbar.

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Toolbar = () => {
-  return <div>Toolbar</div>;
+  return (
+    <div>
+      <Link to="/feed">My Feed</Link>
+      <Link to="/liked">My Hearts</Link>
+      <Link to="/posts/create">Create Post</Link>
+    </div>
+  );
 };
 
 export default Toolbar;
