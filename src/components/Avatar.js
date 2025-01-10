277e6c032ccc78e11b5ab46ea945bb779e82f134
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 11:55:56 +0000

    Feat: Added styling class to Avatar image.

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styles from "./Avatar.module.css";
 
 const Avatar = ({ src, height = 45, text }) => {
   return (
     <span>
-      <img src={src} width={height} height={height} alt="avatar" />
+      <img
+        className={styles.Avatar}
+        src={src}
+        width={height}
+        height={height}
+        alt="avatar"
+      />
       {text}
     </span>
   );
 };
 
 export default Avatar;
+
+
