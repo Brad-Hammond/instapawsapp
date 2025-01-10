41c4185a7da7e1e4d446ea90a39547968561a57e
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 12:58:42 +0000

    Feat: Added link to app.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import NavBar from "./components/NavBar";
+import LogInForm from "./pages/auth/LogInForm";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import './api/axiosDefault';
 
