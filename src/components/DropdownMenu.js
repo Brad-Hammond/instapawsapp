623f3861e43e6356b3f5a97d0be9999a511a4e69
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 15:36:26 +0000

    Feat: Added DropdownMenu within profilepage.

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -24,33 +24,73 @@ const DropdownSelect = React.forwardRef(
 );
 
 export const DropdownMenu = ({ handleEdit, handleDelete }) => {
-    return (
-      <Dropdown className="mr-auto" drop="right">
-        <Dropdown.Toggle as={DropdownSelect} />
-  
-        <Dropdown.Menu
-          className="text-center"
-          popperConfig={{ strategy: "fixed" }}
+  return (
+    <Dropdown className="mr-auto" drop="right">
+      <Dropdown.Toggle as={DropdownSelect} />
+
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <OverlayTrigger placement="top" overlay={<Tooltip>Delete</Tooltip>}>
+          <Dropdown.Item
+            className={styles.DropdownItem}
+            onClick={handleDelete}
+            aria-label="remove post"
+          >
+            <i className="fas fa-eraser" />
+          </Dropdown.Item>
+        </OverlayTrigger>
+        <OverlayTrigger placement="top" overlay={<Tooltip>Edit</Tooltip>}>
+          <Dropdown.Item
+            className={styles.DropdownItem}
+            onClick={handleEdit}
+            aria-label="edit or change post"
+          >
+            <i className="fas fa-cogs" />
+          </Dropdown.Item>
+        </OverlayTrigger>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export function ProfileEditDropdownMenu({ id }) {
+  const history = useHistory();
+  return (
+    <Dropdown className="ml-auto" drop="left">
+      <Dropdown.Toggle as={DropdownSelect} />
+
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip>Edit your profile</Tooltip>}
         >
-          <OverlayTrigger placement="top" overlay={<Tooltip>Delete</Tooltip>}>
-            <Dropdown.Item
-              className={styles.DropdownItem}
-              onClick={handleDelete}
-              aria-label="remove post"
-            >
-              <i className="fas fa-eraser" />
-            </Dropdown.Item>
-          </OverlayTrigger>
-          <OverlayTrigger placement="top" overlay={<Tooltip>Edit</Tooltip>}>
-            <Dropdown.Item
-              className={styles.DropdownItem}
-              onClick={handleEdit}
-              aria-label="edit or change post"
-            >
-              <i className="fas fa-cogs" />
-            </Dropdown.Item>
-          </OverlayTrigger>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  };
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit`)}
+            aria-label="edit profile"
+            className={styles.DropdownItem}
+          >
+            <i className="fas fa-cogs" />
+          </Dropdown.Item>
+        </OverlayTrigger>
+
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip>Change your password</Tooltip>}
+        >
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/password`)}
+            aria-label="change password"
+            className={styles.DropdownItem}
+          >
+            <i className="fas fa-user-lock" />
+          </Dropdown.Item>
+        </OverlayTrigger>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
