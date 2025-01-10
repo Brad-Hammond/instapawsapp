252b6ae98591c94ddea2b3e9d3e6514283abf43b
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 14:20:03 +0000

    Feat: Updated App.js with styles and login route.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
+import './App.module.css';
 import NavBar from "./components/NavBar";
 import LogInForm from "./pages/auth/LogInForm";
+import HomePage from "./components/HomePage";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import './api/axiosDefault';
 
 // Placeholder Components for Routes
-const Home = () => <div>Home Page</div>;
-const Login = () => <div>Login Page</div>;
 const SignUp = () => <div>Sign Up Page</div>;
 const CreatePost = () => <div>Create Post Page</div>;
 const Profile = () => <div>Profile Page</div>;
@@ -20,11 +19,13 @@ function App() {
         <CurrentUserProvider>
           <NavBar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dj-rest-auth/login" element={<Login />} />
-            <Route path="/dj-rest-auth/registration" element={<SignUp />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LogInForm />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/posts/create" element={<CreatePost />} />
             <Route path="/profiles/:id" element={<Profile />} />
+            <Route path="/dj-rest-auth/login" element={<LogInForm />} />
+            <Route path="/dj-rest-auth/registration" element={<SignUp />} />
           </Routes>
         </CurrentUserProvider>
       </Router>
