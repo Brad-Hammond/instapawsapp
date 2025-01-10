f2565ca458b09f4a640536dc66d3b329b27f1c8a
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 15:35:28 +0000

    Feat: Added DropdownMenu for post owners to and - edit post calls.

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useHistory } from "react-router";
 import styles from "../styles/DropdownMenu.module.css";
+import Dropdown from "react-bootstrap/Dropdown";
 import { TbGridDots } from "react-icons/tb";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const DropdownSelect = React.forwardRef(
   ({ onClick }, ref) => (
@@ -19,4 +23,34 @@ const DropdownSelect = React.forwardRef(
   )
 );
 
-export default DropdownSelect;
+export const DropdownMenu = ({ handleEdit, handleDelete }) => {
+    return (
+      <Dropdown className="mr-auto" drop="right">
+        <Dropdown.Toggle as={DropdownSelect} />
+  
+        <Dropdown.Menu
+          className="text-center"
+          popperConfig={{ strategy: "fixed" }}
+        >
+          <OverlayTrigger placement="top" overlay={<Tooltip>Delete</Tooltip>}>
+            <Dropdown.Item
+              className={styles.DropdownItem}
+              onClick={handleDelete}
+              aria-label="remove post"
+            >
+              <i className="fas fa-eraser" />
+            </Dropdown.Item>
+          </OverlayTrigger>
+          <OverlayTrigger placement="top" overlay={<Tooltip>Edit</Tooltip>}>
+            <Dropdown.Item
+              className={styles.DropdownItem}
+              onClick={handleEdit}
+              aria-label="edit or change post"
+            >
+              <i className="fas fa-cogs" />
+            </Dropdown.Item>
+          </OverlayTrigger>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  };
