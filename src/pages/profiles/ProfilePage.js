4f4765f5cd8851d0de98a4a97ac32b314198b82e
Brad-Hammond <brad_hammond1@sky.com>
2025-01-10 15:45:15 +0000

    Feat:Added infinite scrolling to display posts - made by the profile owner.

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -48,56 +48,112 @@ function ProfilePage() {
     };
     fetchData();
   }, [id, setProfileData]);
+
+  const profile = profilePage.results[0];
+  const is_owner = currentUser?.username === profile?.owner;
+
+  const profileDetail = (
+    <>
+      <Row noGutters className="p-3 text-center">
+        <Col lg={4} className="text-lg-left">
+          <Image
+            roundedCircle
+            className={styles.UserAvatar}
+            src={profile?.image}
+            alt="Profile avatar"
+          />
+        </Col>
+        <Col lg={4}>
+          <h3 className="mt-2 mb-4">{profile?.owner}&apos;s profile</h3>
+          <p className={styles.UserSocialNumbers}>
+            Posts <span>{profile?.posts_total}</span>
+          </p>
+          <p className={styles.UserSocialNumbers}>
+            Followers <span>{profile?.followers_total}</span>
+          </p>
+          <p className={styles.UserSocialNumbers}>
+            Following <span>{profile?.following_total}</span>
+          </p>
+        </Col>
+        <Col lg={4} className="text-lg-right">
+          {profile?.is_owner && <ProfileEditDropdownMenu id={profile?.id} />}
+          {currentUser &&
+            !is_owner &&
+            (profile?.following_id ? (
+              <Button
+                className={`${styles.UnfollowButton}`}
+                onClick={() => handleUnfollow(profile)}
+              >
+                unfollow
+              </Button>
+            ) : (
+              <Button
+                className={`${styles.FollowButton}`}
+                onClick={() => handleFollow(profile)}
+              >
+                follow
+              </Button>
+            ))}
+        </Col>
+        {profile?.content && <Col className="p-2">{profile.content}</Col>}
+      </Row>
+    </>
+  );
+
+  const profileDetailPosts = (
+    <>
+      <hr />
+      <p className={`${styles.UserPostsTitle} text-center `}>
+        Posts by <strong>{profile?.owner}</strong>
+      </p>
+      {profilePosts.results.length ? (
+        <InfiniteScroll
+          dataLength={profilePosts.results.length}
+          loader={<Asset spinner />}
+          hasMore={!!profilePosts.next}
+          next={() => fetchMoreData(profilePosts, setProfilePosts)}
+        >
+          {profilePosts.results.map((post) => (
+            <Post key={post.id} {...post} setPosts={setProfilePosts} />
+          ))}
+        </InfiniteScroll>
+      ) : (
+        <Asset
+          src={NoResultsFoundImage}
+          message={`It looks like ${profile?.owner} hasn't posted yet... :(`}
+        />
+      )}
+      <hr />
+    </>
+  );
+
+  return (
+    <CSSTransition in={true} appear={true} timeout={{ enter: 300 }} classNames="fade">
+      <Container>
+        <Row>
+          <Col className="pt-2 p-0 g-0" lg={3}>
+            <Toolbar />
+            <Container className={`${appStyles.Content} mb-2`}>
+              <PopularProfiles />
+            </Container>
+          </Col>
+
+          <Col className="py-2 p-0 p-lg-2" lg={9}>
+            <Container className={appStyles.Content}>
+              {hasLoaded ? (
+                <>
+                  {profileDetail}
+                  {profileDetailPosts}
+                </>
+              ) : (
+                <Asset spinner />
+              )}
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </CSSTransition>
+  );
 }
 
-const profile = profilePage.results[0];
-const is_owner = currentUser?.username === profile?.owner;
-
-const profileDetail = (
-  <>
-    <Row noGutters className="p-3 text-center">
-      <Col lg={4} className="text-lg-left">
-        <Image
-          roundedCircle
-          className={styles.UserAvatar}
-          src={profile?.image}
-          alt="Profile avatar"
-        />
-      </Col>
-      <Col lg={4}>
-        <h3 className="mt-2 mb-4">{profile?.owner}&apos;s profile</h3>
-        <p className={styles.UserSocialNumbers}>
-          Posts <span>{profile?.posts_total}</span>
-        </p>
-        <p className={styles.UserSocialNumbers}>
-          Followers <span>{profile?.followers_total}</span>
-        </p>
-        <p className={styles.UserSocialNumbers}>
-          Following <span>{profile?.following_total}</span>
-        </p>
-      </Col>
-      <Col lg={4} className="text-lg-right">
-        {profile?.is_owner && <ProfileEditDropdownMenu id={profile?.id} />}
-        {currentUser &&
-          !is_owner &&
-          (profile?.following_id ? (
-            <Button
-              className={`${styles.UnfollowButton}`}
-              onClick={() => handleUnfollow(profile)}
-            >
-              unfollow
-            </Button>
-          ) : (
-            <Button
-              className={`${styles.FollowButton}`}
-              onClick={() => handleFollow(profile)}
-            >
-              follow
-            </Button>
-          ))}
-      </Col>
-      {profile?.content && <Col className="p-2">{profile.content}</Col>}
-    </Row>
-  </>
-);
-
+export default ProfilePage;
