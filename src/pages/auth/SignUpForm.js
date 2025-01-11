564fd755cfd17cdf6f49c7e7b733db2367c787fe
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 16:11:26 +0000

    Feat: Refactored  SignUpForm: Add CSSTransition - for animations, improve error handling, and enhance styling and user feedback.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,17 +1,21 @@
-import { Link } from "react-router-dom";
 import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
+import { useRedirect } from "../../hooks/useRedirect";
 import styles from "../../styles/SignupLogIn.module.css";
-import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
-import { useNavigate } from "react-router-dom";
-import { TbUserPlus } from "react-icons/tb"; 
-import { Container, Row, Col, Form, Button, Alert, Image } from "react-bootstrap";
-import { useCookies } from "react-cookie";
-import { jwtDecode } from "jwt-decode";
+import btnStyles from "../../styles/HomePage.module.css";
+import CSSTransition from "react-transition-group/CSSTransition";
+import { TbUserPlus } from "react-icons/tb";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
 
 const SignUpForm = () => {
-  const setCurrentUser = useSetCurrentUser();
-  const navigate = useNavigate();
+  useRedirect("loggedIn");
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
@@ -19,7 +23,7 @@ const SignUpForm = () => {
   });
   const { username, password1, password2 } = signUpData;
   const [errors, setErrors] = useState({});
-  const [, setCookie] = useCookies(["refreshTokenTimestamp"]);
+  const history = useHistory();
 
   const handleChange = (e) => {
     setSignUpData({
@@ -28,109 +32,130 @@ const SignUpForm = () => {
     });
   };
 
-  const setAuthToken = (data) => {
-    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
-    setCookie("refreshTokenTimestamp", refreshTokenTimestamp);
-    setCookie("profile_id", data?.user.profile_id);
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await axios.post("/dj-rest-auth/registration/", signUpData);
-      navigate("/login");
+      history.push("/login");
     } catch (err) {
       setErrors(err.response?.data);
     }
   };
-
   return (
-    <Row className={styles.Row}>
-      <Col className={`${styles.Col} text-center`}>
-        <Container>
-          <h1 className={styles.Header}>
-            Sign Up <TbUserPlus />
-          </h1>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="text"
-                placeholder="Enter username"
-                name="username"
-                value={signUpData.username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant="danger" className={styles.AlertStyles} key={idx}>
-                {message}
-              </Alert>
-            ))}
+    <CSSTransition
+      in={true}
+      appear={true}
+      timeout={{ enter: 300 }}
+      classNames="fade"
+    >
+      <Row className={styles.Row}>
+        <Col className={`${styles.Col} text-center`}>
+          <Container>
+            <h1 className={styles.Header}>
+              Sign Up <TbUserPlus />
+            </h1>
 
-            <Form.Group controlId="password1">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="Enter password"
-                name="password1"
-                value={signUpData.password1}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password1?.map((message, idx) => (
-              <Alert variant="danger" className={styles.AlertStyles} key={idx}>
-                {message}
-              </Alert>
-            ))}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="username">
+                <Form.Label className="d-none">Username</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="username"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert
+                  variant="danger"
+                  className={styles.AlertStyles}
+                  key={idx}
+                >
+                  {message}
+                </Alert>
+              ))}
 
-            <Form.Group controlId="password2">
-              <Form.Label className="d-none">Confirm Password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="Confirm password"
-                name="password2"
-                value={signUpData.password2}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password2?.map((message, idx) => (
-              <Alert variant="danger" className={styles.AlertStyles} key={idx}>
-                {message}
-              </Alert>
-            ))}
-
-            <Button
-              className={`${styles.LogInSignUpButton} ${styles.HomeButton}`}
-              type="submit"
+              <Form.Group controlId="password1">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="password"
+                  name="password1"
+                  value={password1}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password1?.map((message, idx) => (
+                <Alert
+                  variant="danger"
+                  className={styles.AlertStyles}
+                  key={idx}
+                >
+                  {message}
+                </Alert>
+              ))}
+              <Form.Group controlId="password2">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="confirm password"
+                  name="password2"
+                  value={password2}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password2?.map((message, idx) => (
+                <Alert
+                  variant="danger"
+                  className={styles.AlertStyles}
+                  key={idx}
+                >
+                  {message}
+                </Alert>
+              ))}
+              <Button
+                className={`${btnStyles.HomeButton} ${styles.LogInSignUpButton}`}
+                type="submit"
+              >
+                Create account!
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <Alert
+                  variant="danger"
+                  className={styles.AlertStyles}
+                  key={idx}
+                >
+                  {message}
+                </Alert>
+              ))}
+            </Form>
+          </Container>
+          <Container className="mb-5">
+            <Link
+              className={`${styles.Link} mt-4 font-weight-bold`}
+              to="/login"
             >
-              Create Account
-            </Button>
-          </Form>
-        </Container>
-
-        <Container className="mb-5">
-          <Link
-            className={`${styles.Link} mt-4 font-weight-bold`}
-            to="/login"
-          >
-            Have an account? Click here to log in.
-          </Link>
-        </Container>
-        <Container>
-          <Col className="text-center">
-            <Image
-              alt="Sign up illustration"
-              className={`${styles.LoginImage}`}
-              src="https://res.cloudinary.com/dpdhjt0cf/image/upload/v1736519045/New-Main-1254140_yaomfl.jpg"
-            />
-          </Col>
-        </Container>
-      </Col>
-    </Row>
+              Have an account? Click here to log in.
+            </Link>
+          </Container>
+          <Container>
+            <Col className="text-center">
+              <Image
+                alt="Cartoon Fitness blog sign up image"
+                className={`${styles.LoginImage} text-center`}
+                src={
+                  "https://res.cloudinary.com/drhfh23tl/image/upload/v1679416297/login-img_hqathg.png"
+                }
+              />
+            </Col>
+          </Container>
+        </Col>
+      </Row>
+    </CSSTransition>
   );
 };
 
