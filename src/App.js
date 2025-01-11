e54542ae303228c94870116a31fcc4f4887630fa
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 16:53:33 +0000

    Feat: Updated app.js to include all the new files.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,73 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.module.css';
+import { Routes, Route } from "react-router-dom";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
+import "./api/axiosDefault";
+import styles from "../src/App.module.css";
+import Container from "react-bootstrap/Container";
 import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
 import LogInForm from "./pages/auth/LogInForm";
 import SignUpForm from "./pages/auth/SignUpForm";
-import HomePage from "./components/HomePage";
 import ProfilePage from "./pages/profiles/ProfilePage";
-import PopularProfiles from "./pages/profiles/PopularProfiles";
-import Profiles from "./pages/profiles/Profile";
-import { DropdownMenu } from "./components/DropdownMenu";
-import Toolbar from "./components/Toolbar";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-import { CurrentUserProvider } from "./contexts/CurrentUserContext";
-import './api/axiosDefault';
-
-// Placeholder Components for Routes
-const CreatePost = () => <div>Create Post Page</div>;
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
+import GeneralPostsPage from "./pages/posts/GeneralPostsPage";
+import PostEditForm from "./pages/posts/PostEditForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
-    <div className="App">
-      <Router>
-        <CurrentUserProvider>
-          <NavBar />
-          <DropdownMenu />
-          <Toolbar />
-          <ScrollToTopButton />
+    <div className={`${styles.App} psychic`}>
+      <NavBar />
+      <Container className={styles.Main}>
+        {!currentUser ? (
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LogInForm />} />
             <Route path="/signup" element={<SignUpForm />} />
-            <Route path="/posts/create" element={<CreatePost />} />
-            <Route path="/profiles" element={<Profiles />} />
-            <Route path="/profiles/:id" element={<ProfilePage />} />
-            <Route path="/popular-profiles" element={<PopularProfiles />} />
-            <Route path="/dj-rest-auth/login" element={<LogInForm />} />
-            <Route path="/dj-rest-auth/registration" element={<SignUpForm />} />
+            <Route path="*" element={<HomePage />} />
           </Routes>
-        </CurrentUserProvider>
-      </Router>
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <GeneralPostsPage message="No results found, try another keyword or tag?" />
+              }
+            />
+            <Route
+              path="/feed"
+              element={
+                <GeneralPostsPage
+                  message="No results found, try another keyword or follow someone."
+                  filter={`owner__followed__owner__profile=${profile_id}&`}
+                />
+              }
+            />
+            <Route
+              path="/liked"
+              element={
+                <GeneralPostsPage
+                  message="No results found, try another keyword or like a post."
+                  filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                />
+              }
+            />
+            <Route path="/posts/create" element={<PostCreateForm />} />
+            <Route path="/posts/:id" element={<PostPage />} />
+            <Route path="/posts/:id/edit" element={<PostEditForm />} />
+            <Route path="/profiles/:id" element={<ProfilePage />} />
+            <Route path="/profiles/:id/edit/password" element={<UserPasswordForm />} />
+            <Route path="/profiles/:id/edit" element={<ProfileEditForm />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        )}
+      </Container>
     </div>
   );
 }
