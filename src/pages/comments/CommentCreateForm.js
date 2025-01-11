fe596b6da90065182b47984627bae04d88ed2b5f
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 15:53:06 +0000

    Feat: Added comment creation functionality with - form submission, state updates, and API integration in CommentCreateForm.

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -10,4 +10,33 @@ import Avatar from "../../components/Avatar";
 function CommentCreateForm(props) {
   const { profileImage, profile_id, post, setPost, setComments } = props;
   const [comment_info, setCommentInfo] = useState("");
+
+  const handleChange = (e) => {
+    setCommentInfo(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        post,
+        comment_info,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_total: prevPost.results[0].comments_total + 1,
+          },
+        ],
+      }));
+      setCommentInfo("");
+    } catch (err) {
+      return err;
+    }
+  };
 }
