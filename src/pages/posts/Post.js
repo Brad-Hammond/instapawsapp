20aab4451596bd87ac1e460adeab6ecfd90c8bde
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 14:24:06 +0000

    Feat: Added  user information (owner, avatar) to - Post component.

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
+import Media from "react-bootstrap/Media";
+import Avatar from "../../components/Avatar";
 
-const Post = ({ id, title, image, content }) => {
+const Post = ({ id, title, image, content, owner, profile_id, profile_image }) => {
   return (
     <Card>
+      <Card.Body>
+        <Media>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+        </Media>
+      </Card.Body>
       <Link to={`/posts/${id}/`}>
         <Card.Img src={image} alt={title} />
       </Link>
