a37a143330505bbb815fff4d4d6d69511ddcdadd
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 16:00:38 +0000

    Feat: Added form content handling and submission - logic with API integration for updating comments in CommentEditForm.

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -6,4 +6,32 @@ import Form from "react-bootstrap/Form";
 function CommentEditForm(props) {
   const { id, comment_info, setShowEditForm, setComments } = props;
   const [formContent, setFormContent] = useState(comment_info);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        comment_info: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                comment_info: formContent.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      return err;
+    }
+  };
 }
