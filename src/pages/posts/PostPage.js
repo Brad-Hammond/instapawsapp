c3495ddba2ea11b85ae4cc2645cc9d9d0bfc31ed
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 14:48:39 +0000

    Feat: Added responsive layout and styling to - PostPage with Bootstrap containers and columns.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { axiosReq } from "../../api/axiosDefault";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/Post.module.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function PostPage() {
   const { id } = useParams();
@@ -25,17 +30,24 @@ function PostPage() {
   }, [id]);
 
   return (
-    <div>
-      <h1>Post Page for ID: {id}</h1>
-      <div>
-        <h2>Post Data:</h2>
-        <pre>{JSON.stringify(post, null, 2)}</pre>
-      </div>
-      <div>
-        <h2>Comments:</h2>
-        <pre>{JSON.stringify(comments, null, 2)}</pre>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col className="py-2 p-0 p-lg-2" lg={3}>
+          <div className={`${appStyles.Content} mb-3 mt-3`}>
+          </div>
+        </Col>
+        <Col className="py-2 p-0 p-lg-2" lg={6}>
+          <Container className={`${appStyles.Content} mb-2`}>
+            <h2>Post Content</h2>
+            <pre>{JSON.stringify(post.results[0], null, 2)}</pre>
+          </Container>
+          <Container className={appStyles.Content}>
+            <h2>Comments</h2>
+            <pre>{JSON.stringify(comments.results, null, 2)}</pre>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
