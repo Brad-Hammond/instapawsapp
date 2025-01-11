5744d7265c67974aae3d63367efb419d4cda79dc
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 13:52:07 +0000

    Feat: Initialized UserPasswordForm component with- basic imports and structure.

---
+++ src/pages/profiles/UserPasswordForm.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import appStyles from "../../App.module.css";
+import inputStyles from "../../styles/SignupLogIn.module.css";
+import alertStyles from "../../styles/PostCreateEditForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import CSSTransition from "react-transition-group/CSSTransition";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import UserFeedbackCue from "../../components/UserFeedbackCue";
+
+const UserPasswordForm = () => {
+  const history = useHistory();
+  const { id } = useParams();
+  const currentUser = useCurrentUser();
+
+  const [userData, setUserData] = useState({
+    new_password1: "",
+    new_password2: "",
+  });
+  const { new_password1, new_password2 } = userData;
+  const [showPasswordMsg, setPasswordMsg] = useState();
+  const [errors, setErrors] = useState({});
+
+  return <div>UserPasswordForm</div>;
+};
+
+export default UserPasswordForm;
