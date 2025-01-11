2198f07c08b9bfc3b167435ab810fa415900783b
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 14:50:13 +0000

    Feat: Integrated Post and Comment components with-  functionality for displaying and creating comments on PostPage (comments component to come).

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { axiosReq } from "../../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefault";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/Post.module.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import Comment from "../comments/Comment";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
   const [comments, setComments] = useState({ results: [] });
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
     const handleMount = async () => {
@@ -38,12 +43,28 @@ function PostPage() {
         </Col>
         <Col className="py-2 p-0 p-lg-2" lg={6}>
           <Container className={`${appStyles.Content} mb-2`}>
-            <h2>Post Content</h2>
-            <pre>{JSON.stringify(post.results[0], null, 2)}</pre>
+            <Post {...post.results[0]} setPosts={setPost} postPage />
           </Container>
           <Container className={appStyles.Content}>
-            <h2>Comments</h2>
-            <pre>{JSON.stringify(comments.results, null, 2)}</pre>
+            {currentUser ? (
+              <CommentCreateForm
+                profileImage={currentUser.profile_image}
+                profile_id={currentUser.profile_id}
+                post={id}
+                setPost={setPost}
+                setComments={setComments}
+              />
+            ) : (
+              <p>Log in to leave a comment!</p>
+            )}
+            {comments.results.map((comment) => (
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
+            ))}
           </Container>
         </Col>
       </Row>
