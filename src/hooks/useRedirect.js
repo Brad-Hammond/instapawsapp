f4bdc3f863e8a56c8479a8accca1ad6ca972a3a8
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 16:55:18 +0000

    Feat: Changed useHistory to useNavigate.

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,24 +1,24 @@
 import { useEffect } from "react";
-import { useHistory } from "react-router";
+import { useNavigate } from "react-router";
 import axios from "axios";
 
 export const useRedirect = (userAuthStatus) => {
-  const history = useHistory();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleMount = async () => {
       try {
         await axios.post("/dj-rest-auth/token/refresh/");
         if (userAuthStatus === "loggedIn") {
-          history.push("/");
+          navigate.push("/");
         }
       } catch (err) {
         if (userAuthStatus === "loggedOut") {
-          history.push("/");
+          navigate.push("/");
         }
       }
     };
 
     handleMount();
-  }, [history, userAuthStatus]);
+  }, [navigate, userAuthStatus]);
 };
