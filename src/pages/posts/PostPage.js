7c2bff4a700cd7bd6d665069eaa6a2dea07903f0
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 14:47:47 +0000

    Feat: Added data fetching to PostPage component - for post and comments using axiosReq.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefault";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
   const [comments, setComments] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}/`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
     <div>
