a0c89762ba71f37e3b09f09f2a04b4b21fc89ec7
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 15:14:24 +0000

    Feat: Integrated CSSTransition for smooth page - transitions, enhance tag-based filtering, add no-results image, and improve responsiveness and UI structure.

--- src/pages/posts/GeneralPostsPage.js
+++ src/pages/posts/GeneralPostsPage.js
@@ -1,31 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Form } from "react-bootstrap";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { axiosReq } from "../../api/axiosDefaults";
-import { fetchMoreData } from "../../utils/utils";
-import Asset from "../../components/Asset";
-import Toolbar from "../../components/Toolbar";
-import PopularProfiles from "../profiles/PopularProfiles";
-import Post from "./Post";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefault";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/GeneralPostsPage.module.css";
+import CSSTransition from "react-transition-group/CSSTransition";
+import { CgSearch } from "react-icons/cg";
+import Badge from "react-bootstrap/Badge";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Asset from "../../components/Asset";
+import Toolbar from "../../components/Toolbar.js";
+import Post from "./Post";
+import NoResultsImage from "../../assets/noImageFound.avif";
+import InfiniteScroll from "react-infinite-scroll-component";
+import PopularProfiles from "../profiles/PopularProfiles";
+import { fetchMoreData } from "../../utils/utils";
 
-function GeneralPostsPage() {
+function GeneralPostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
-  const [hasLoaded, setHasLoaded] = useState(false);
-  const [query, setQuery] = useState("");
   const [tags, setTags] = useState(null);
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  const currentUser = useCurrentUser();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const { data } = await axiosReq.get(
-          `/posts/?search=${query}${tags ? `&tags=${tags}` : ""}`
+          `/posts/?${filter}search=${query}${
+            tags !== null ? `&tags=${tags}` : ""
+          }`
         );
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
-        console.error(err);
+        return err;
       }
     };
 
@@ -33,60 +47,94 @@ function GeneralPostsPage() {
     const fetchTimer = setTimeout(() => {
       fetchPosts();
     }, 800);
-
-    return () => clearTimeout(fetchTimer);
-  }, [query, tags]);
+    return () => {
+      clearTimeout(fetchTimer);
+    };
+  }, [filter, query, pathname, currentUser, tags]);
 
   return (
-    <Container>
-      <Row>
-        <Col lg={3}>
-          <Toolbar />
-          <Container className={`${appStyles.Content} mb-3 mt-3`}>
-            <PopularProfiles />
-          </Container>
-        </Col>
-        <Col lg={6}>
-          <Form className={styles.SearchBar} onSubmit={(e) => e.preventDefault()}>
-            <Form.Control
-              type="text"
-              className="mr-sm-2"
-              placeholder="Search..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            />
-          </Form>
-          {hasLoaded ? (
-            <InfiniteScroll
-              dataLength={posts.results.length}
-              loader={<Asset spinner />}
-              hasMore={!!posts.next}
-              next={() => fetchMoreData(posts, setPosts)}
+    <CSSTransition
+      in={true}
+      appear={true}
+      timeout={{ enter: 300 }}
+      classNames="fade"
+    >
+      <Container>
+        <Row>
+          <Col className="py-2 p-0 p-lg-2" lg={3}>
+            <Toolbar />
+            <Container className={`${appStyles.Content} mb-3 mt-3`}>
+              <PopularProfiles />
+            </Container>
+          </Col>
+          <Col className="py-2 p-0 p-lg-2" lg={6}>
+            <CgSearch className={styles.SearchIcon} />
+            <Form
+              className={styles.SearchBar}
+              onSubmit={(e) => e.preventDefault()}
             >
-              {posts.results.map((post) => (
-                <Post
-                  key={post.id}
-                  content={
-                    post.content.length > 200
-                      ? post.content.slice(0, 200) + "..."
-                      : post.content
-                  }
-                  {...post}
-                  setPosts={setPosts}
-                />
-              ))}
-            </InfiniteScroll>
-          ) : (
-            <Asset spinner />
-          )}
-        </Col>
-        <Col lg={3}>
-          <Container className={`${appStyles.Content} mb-3 mt-3 d-none d-lg-block`}>
-            <p className={`${styles.PostTags} font-weight-bold text-center`}>
-              <i className={`${styles.TagIcon} fas fa-tag`}></i> Search by Post Tags
-            </p>
-            <Badge variant="primary" pill className={`${styles.Tags}`} onClick={() => setTags(null)}>
-            All Tags
+              <Form.Control
+                type="text"
+                className="mr-sm-2"
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </Form>
+            {hasLoaded ? (
+              <>
+                {posts.results.length ? (
+                  <InfiniteScroll
+                    dataLength={posts.results.length}
+                    loader={<Asset spinner />}
+                    hasMore={!!posts.next}
+                    next={() => fetchMoreData(posts, setPosts)}
+                  >
+                    {posts.results.map((post) => (
+                      <Post
+                        key={post.id}
+                        content={
+                          post.content.length > 200
+                            ? post.content.slice(0, 200) + " ....."
+                            : post.content
+                        }
+                        {...post}
+                        setPosts={setPosts}
+                      />
+                    ))}
+                  </InfiniteScroll>
+                ) : (
+                  <Container className={appStyles.Content}>
+                    <Asset
+                      className={styles.NoResultsImage}
+                      src={NoResultsImage}
+                      width={40}
+                      height={40}
+                      message={message}
+                    />
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset spinner />
+              </Container>
+            )}
+          </Col>
+          <Col className="py-2 p-0 p-lg-2" lg={3}>
+            <Container
+              className={`${appStyles.Content} mb-3 mt-3 d-none d-lg-block`}
+            >
+              <p className={`${styles.PostTags} font-weight-bold text-center`}>
+              <i className={`${styles.TagIcon} fas fa-tag`} ></i> Search by Post Tags
+              </p>
+              <Badge
+                variant="primary"
+                pill
+                className={`${styles.Tags}`}
+                onClick={() => setTags(null)}
+              >
+                All Tags
               </Badge>
               <Badge
                 variant="primary"
@@ -152,10 +200,11 @@ function GeneralPostsPage() {
               >
                 Walks
               </Badge>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </CSSTransition>
   );
 }
 
