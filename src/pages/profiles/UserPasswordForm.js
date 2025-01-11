9b0f9cf98c0bb936d9023fbf834670d449d6680d
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 14:00:54 +0000

    Feat: Finalized code for UserPasswordForm.

--- src/pages/profiles/UserPasswordForm.js
+++ src/pages/profiles/UserPasswordForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefault";
 import appStyles from "../../App.module.css";
 import inputStyles from "../../styles/SignupLogIn.module.css";
 import alertStyles from "../../styles/PostCreateEditForm.module.css";
@@ -34,106 +35,102 @@ const UserPasswordForm = () => {
     });
   };
 
-  return <div>UserPasswordForm</div>;
-};
-
-useEffect(() => {
+  useEffect(() => {
     if (currentUser?.profile_id?.toString() !== id) {
       history.push("/");
     }
   }, [currentUser, history, id]);
-  
-const handleSubmit = async (event) => {
-  event.preventDefault();
-  try {
-    await axiosRes.post("/dj-rest-auth/password/change/", userData);
-    setPasswordMsg(true);
-    setTimeout(function () {
-      history.goBack();
-    }, 2000);
-  } catch (err) {
-    setErrors(err.response?.data);
-  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.post("/dj-rest-auth/password/change/", userData);
+      setPasswordMsg(true);
+      setTimeout(function () {
+        history.goBack();
+      }, 2000);
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <CSSTransition
+      in={true}
+      appear={true}
+      timeout={{ enter: 300 }}
+      classNames="fade"
+    >
+      <Row>
+        <Col className="py-2 mx-auto text-center font-weight-bold" md={10}>
+          {showPasswordMsg && (
+            <UserFeedbackCue
+              variant="Info"
+              message="Password changed! Taking you back!"
+            />
+          )}
+          <Container className={appStyles.Content}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label>New password</Form.Label>
+                <Form.Control
+                  placeholder="type your new password"
+                  type="password"
+                  value={new_password1}
+                  onChange={handleChange}
+                  name="new_password1"
+                  className={`${inputStyles.Input} text-center`}
+                />
+              </Form.Group>
+              {errors?.new_password1?.map((message, idx) => (
+                <Alert
+                  variant="warning"
+                  className={alertStyles.AlertStyles}
+                  key={idx}
+                >
+                  {message}
+                </Alert>
+              ))}
+              <Form.Group>
+                <Form.Label>Confirm new password</Form.Label>
+                <Form.Control
+                  placeholder="confirm new password"
+                  type="password"
+                  value={new_password2}
+                  onChange={handleChange}
+                  name="new_password2"
+                  className={`${inputStyles.Input} text-center`}
+                />
+              </Form.Group>
+              {errors?.new_password2?.map((message, idx) => (
+                <Alert
+                  variant="warning"
+                  className={alertStyles.AlertStyles}
+                  key={idx}
+                >
+                  {message}
+                </Alert>
+              ))}
+              <Button
+                type="submit"
+                className={`mx-2 my-2 ${btnStyles.Button}`}
+                onMouseDown={(event) => event.preventDefault()}
+              >
+                Save new password
+              </Button>
+              <Button
+                onMouseDown={(event) => event.preventDefault()}
+                className={`mx-2 ${btnStyles.CancelButton}`}
+                onClick={() => history.goBack()}
+              >
+                Go back
+              </Button>
+            </Form>
+          </Container>
+        </Col>
+      </Row>
+    </CSSTransition>
+  );
 };
-
-return (
-  <CSSTransition
-    in={true}
-    appear={true}
-    timeout={{ enter: 300 }}
-    classNames="fade"
-  >
-    <Row>
-      <Col className="py-2 mx-auto text-center font-weight-bold" md={10}>
-        {showPasswordMsg && (
-          <UserFeedbackCue
-            variant="Info"
-            message="Password changed! Taking you back!"
-          />
-        )}
-        <Container className={appStyles.Content}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Label>New password</Form.Label>
-              <Form.Control
-                placeholder="type your new password"
-                type="password"
-                value={new_password1}
-                onChange={handleChange}
-                name="new_password1"
-                className={`${inputStyles.Input} text-center`}
-              />
-            </Form.Group>
-            {errors?.new_password1?.map((message, idx) => (
-              <Alert
-                variant="warning"
-                className={alertStyles.AlertStyles}
-                key={idx}
-              >
-                {message}
-              </Alert>
-            ))}
-            <Form.Group>
-              <Form.Label>Confirm new password</Form.Label>
-              <Form.Control
-                placeholder="confirm new password"
-                type="password"
-                value={new_password2}
-                onChange={handleChange}
-                name="new_password2"
-                className={`${inputStyles.Input} text-center`}
-              />
-            </Form.Group>
-            {errors?.new_password2?.map((message, idx) => (
-              <Alert
-                variant="warning"
-                className={alertStyles.AlertStyles}
-                key={idx}
-              >
-                {message}
-              </Alert>
-            ))}
-            <Button
-              type="submit"
-              className={`mx-2 my-2 ${btnStyles.Button}`}
-              onMouseDown={(event) => event.preventDefault()}
-            >
-              Save new password
-            </Button>
-            <Button
-              onMouseDown={(event) => event.preventDefault()}
-              className={`mx-2 ${btnStyles.CancelButton}`}
-              onClick={() => history.goBack()}
-            >
-              Go back
-            </Button>
-          </Form>
-        </Container>
-      </Col>
-    </Row>
-  </CSSTransition>
-);
-
-
 
 export default UserPasswordForm;
