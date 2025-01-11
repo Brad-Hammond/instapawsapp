2aa7ebf5c3a268f3a821aa262141c40508673aa7
Brad-Hammond <brad_hammond1@sky.com>
2025-01-11 15:23:55 +0000

    Feat: Initialized PostCreateForm component with - state management, form structure, and routing setup.

---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,36 @@
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefault";
+import { useRedirect } from "../../hooks/useRedirect";
+import Asset from "../../components/Asset";
+import Upload from "../../assets/upload-img.png";
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import inputStyles from "../../styles/SignupLogIn.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import formStyles from "../../styles/PostCreateEditForm.module.css";
+import assetStyles from "../../styles/Asset.module.css";
+import CSSTransition from "react-transition-group/CSSTransition";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
+function PostCreateForm() {
+  useRedirect("loggedOut");
+  const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    title: "",
+    tags: "",
+    content: "",
+    image: "",
+  });
+
+  const { title, tags, content, image } = postData;
+  const imageInput = useRef(null);
+  const history = useHistory();
+}
