a67680625a0134e466c758d158899622bed020d6
Brad-Hammond <brad_hammond1@sky.com>
2025-01-13 00:10:35 +0000

    Feat: Installed eslint for testing.

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
+  pluginReact.configs.flat.recommended,
+];
